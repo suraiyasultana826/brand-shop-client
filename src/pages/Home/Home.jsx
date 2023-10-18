9099518072dac9fe2e581d41a048e43eead237a5
suraiyasultana826 <suraiyasultana826@gmail.com>
2023-10-18 21:01:55 +0600

    added banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,26 @@
-
+import banner from '../../assets/Lamborghini-Lanzador-Drive-Featured-Gear.webp'
 const Home = () => {
     return (
-        <div>
-            <h3>this is home</h3>
+        <div className="w-3/4 mx-auto">
+            <div className='w-full h-screen mb-20 bg-no-repeat bg-cover  ' style={{backgroundImage: `url(${banner})`}}>
+               <div className='backdrop-brightness-50'>
+               <div className='text-white font-semibold lg:text-3xl w-3/4 lg:p-40 '>
+                    <hr />
+                    <div className='p-6'>
+                    <p>Attract, Engage & Convert
+                    <br />
+                   <span className='lg:text-4xl text-blue-600 lg:pl-28 '> More </span> <br />
+                    qualified vehicle shoppers</p>
+                    </div>
+                    <hr />
+                    <br />
+                    <br />
+                    <br />
+                    
+                </div>
+               </div>
+
+            </div>
             
         </div>
     );
